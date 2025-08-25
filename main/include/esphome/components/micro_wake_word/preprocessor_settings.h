642bfcb5f290111ae62866b95dcab7e040ddecc1
Tianshuang Ke <dske@listenai.com>
2025-08-25 15:19:34 +0800

    fix(build):fix build with idf v5.5
    style:format code style.
    
    Signed-off-by: Tianshuang Ke <dske@listenai.com>

---
+++ main/include/esphome/components/micro_wake_word/preprocessor_settings.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace esphome
+{
+    namespace micro_wake_word
+    {
+
+        // The number of features the audio preprocessor generates per slice
+        static const uint8_t PREPROCESSOR_FEATURE_SIZE = 40;
+        // Duration of each slice used as input into the preprocessor
+        static const uint8_t FEATURE_DURATION_MS = 30;
+        // Audio sample frequency in hertz
+        static const uint16_t AUDIO_SAMPLE_FREQUENCY = 16000;
+
+    } // namespace micro_wake_word
+} // namespace esphome
